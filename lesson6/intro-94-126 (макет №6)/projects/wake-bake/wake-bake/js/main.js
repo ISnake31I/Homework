335e91849b30d8b23ef489be3215974dc46ca184
ISnake31I <artemagapov312@gmail.com>
2024-11-09 20:40:53 +0800

    Add files via upload

--- lesson6/intro-94-126 (макет №6)/projects/wake-bake/wake-bake/js/main.js
+++ lesson6/intro-94-126 (макет №6)/projects/wake-bake/wake-bake/js/main.js
@@ -1,45 +1,45 @@
 (function () {
 
-// Burger
+    // Burger
 
-    document .addEventListener ('click', burgerInit)
+    document.addEventListener('click', burgerInit)
 
-    function burgerInit (e) {
+    function burgerInit(e) {
 
-        const burgerIcon = e.target.closest ('.burger-icon')
-        const burgerNavLink = e.target.closest ('.nav__link')
-        
+        const burgerIcon = e.target.closest('.burger-icon')
+        const burgerNavLink = e.target.closest('.nav__link')
+
         if (!burgerIcon && !burgerNavLink) return
         if (document.documentElement.clientWidth > 900) return
 
-        if (!document.body.classList.contains ('body--opened-menu')) {
-            document.body.classList.add ('body--opened-menu')
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
         } else {
-            document.body.classList.remove ('body--opened-menu')
+            document.body.classList.remove('body--opened-menu')
         }
 
     }
 
     // Modal
 
-    const modal = document.querySelector ('.modal')
-    const modalButton = document.querySelector ('.about__img-button')
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
 
-    modalButton.addEventListener ('click', openModal)
-    modal.addEventListener ('click', closeModal)
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
 
-    function openModal (e) {
-        e.preventDefault ()
-        document.body.classList.toggle ('body--opened-modal')
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
     }
 
-    function closeModal (e) {
-        e.preventDefault ()
+    function closeModal(e) {
+        e.preventDefault()
 
         const target = e.target
 
-        if (target.closest ('.modal__cancel') || target.classList.contains ('modal')) {
-            document.body.classList.remove ('body--opened-modal')
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
         }
 
 
@@ -47,7 +47,7 @@
 
     // Tabs
 
-    const tabControls = document.querySelector ('.tab-controls')
+    const tabControls = document.querySelector('.tab-controls')
 
     tabControls.addEventListener('click', toggleTab)
 
@@ -72,7 +72,7 @@
             activeContent.classList.remove('tabs-content--show')
         }
 
-        
+
         tabControl.classList.add('tab-controls__link--active')
         tabContent.classList.add('tabs-content--show')
 
@@ -81,29 +81,84 @@
 
     // Accordion
 
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         const controls = document.querySelectorAll('.accordion-list__control');
-    
+
         controls.forEach(control => {
             control.addEventListener('click', () => {
                 const parentItem = control.closest('.accordion__list-item');
                 const content = parentItem.querySelector('.accordion-list__content');
-    
-                // Закрываем остальные элементы
+
                 document.querySelectorAll('.accordion__list-item').forEach(item => {
                     if (item !== parentItem) {
                         item.classList.remove('accordion__list-item--open');
-                        
+
                         const itemContent = item.querySelector('.accordion-list__content');
-                        itemContent.style.maxHeight = null; // Скрываем содержимое
+                        itemContent.style.maxHeight = null;
                     }
                 });
-    
-                // Переключаем текущее содержимое
+
                 const isOpen = parentItem.classList.toggle('accordion__list-item--open');
-                content.style.maxHeight = isOpen ? content.scrollHeight + 'px' : null; // Устанавливаем max-height
+                content.style.maxHeight = isOpen ? content.scrollHeight + 'px' : null;
             });
         });
+    });
+
+    // slider-gallary
+
+    const swiperGallary = new Swiper('.gallary__slider', {
+        spaceBetween: 16,
+        slidesPerView: 1.5,
+        centeredSlides: true,
+
+        pagination: {
+            el: '.gallary__pagination',
+            type: 'fraction',
+        },
+
+        navigation: {
+            prevEl: '.gallary__prev',
+            nextEl: '.gallary__next',
+        },
+
+        breakpoints: {
+            451: {
+                slidesPerView: 2,
+                centeredSlides: false,
+            },
+
+            601: {
+                slidesPerView: 3,
+                centeredSlides: false,
+            },
+
+            801: {
+                spaceBetween: 32,
+                slidesPerView: 4,
+                centeredSlides: false,
+            }
+
+        }
+
+    });
+
+    // slider-testimonials
+
+    const swiperTestimonials = new Swiper('.testimonials__slider', {
+        spaceBetween: 0,
+        slidesPerView: 2,
+        centeredSlides: true,
+
+        navigation: {
+            prevEl: '.testimonials__prev',
+            nextEl: '.testimonials__next',
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+
     });
 
 
